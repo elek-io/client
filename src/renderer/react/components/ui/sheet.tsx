c374edc30244a8744750b706fa519583fe8f8c1b
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-04-11 14:39:10 +0200

    fix: sheets and dialogs are not part of the draggable area

--- src/renderer/react/components/ui/sheet.tsx
+++ src/renderer/react/components/ui/sheet.tsx
@@ -24,7 +24,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      'fixed inset-0 z-50 bg-white/80 dark:bg-black/80 backdrop-blur data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'window-not-draggable-area fixed inset-0 z-50 bg-white/80 dark:bg-black/80 backdrop-blur data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className
     )}
     {...props}
