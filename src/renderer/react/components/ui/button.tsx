84b1b95f1781b4d7c943e5940587c79efddce38b
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-04-12 15:29:59 +0200

    Small color adjustment for primary button and page header

--- src/renderer/react/components/ui/button.tsx
+++ src/renderer/react/components/ui/button.tsx
@@ -9,8 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          'bg-zinc-900 text-zinc-50 shadow hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90',
+        default: 'bg-brand-600 text-white shadow hover:bg-brand-600/90',
         destructive:
           'bg-red-500 text-zinc-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90',
         outline:
--- src/renderer/react/components/ui/page.tsx
+++ src/renderer/react/components/ui/page.tsx
@@ -25,7 +25,7 @@ export const Page = React.forwardRef<HTMLElement, PageProps>(
         onDragEnter={props.onDragEnter}
         onDrop={props.onDrop}
       >
-        <div className="relative bg-brand-50 dark:bg-brand-950 text-zinc-900 dark:text-white pb-32">
+        <div className="relative bg-brand-950 text-white pb-32">
           <div className="relative container mx-auto">
             <div className="md:flex md:items-center px-4 sm:px-6 lg:px-8 py-10">
               <div className="md:flex-auto">
