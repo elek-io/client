51a1a97cc331cc1f0a3b5c084ef96539da751739
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-05-21 01:28:52 +0200

    Using default value definition form to reduce number of duplicated fields

--- src/renderer/react/components/forms/number-value-definition-form.tsx
+++ src/renderer/react/components/forms/number-value-definition-form.tsx
@@ -13,154 +13,102 @@ import {
 } from '../ui/form';
 import { Input } from '../ui/input';
 import { Switch } from '../ui/switch';
-import { Textarea } from '../ui/textarea';
+import { DefaultValueDefinitionForm } from './default-value-definition-form';
 import { setValueAsNumber } from './util';
 
 export interface NumberValueDefinitionFormProps
   extends React.HTMLAttributes<HTMLFormElement> {
   state: UseFormReturn<NumberValueDefinition>;
+  supportedLanguages: SupportedLanguage[];
   currentLanguage: SupportedLanguage;
 }
 
 const NumberValueDefinitionForm = React.forwardRef<
   HTMLFormElement,
   NumberValueDefinitionFormProps
->(({ className, state, currentLanguage, ...props }, ref) => {
+>((props, ref) => {
   return (
-    <Form {...state}>
+    <Form {...props.state}>
       <form className="space-y-6">
-        <FormField
-          control={state.control}
-          name={`name.${currentLanguage}`}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={state.control}
-          name={`description.${currentLanguage}`}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Textarea {...field} />
-              </FormControl>
-              <FormDescription>
-                Describe what to input into this field. This text will be
-                displayed under the field to guide users.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={state.control}
-          name={`defaultValue`}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Default value</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  {...state.register('defaultValue', {
-                    setValueAs: setValueAsNumber,
-                  })}
-                  type="number"
-                />
-              </FormControl>
-              <FormDescription>
-                The initial value for the field.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <div className="flex flex-row items-center justify-between space-x-2">
+        <DefaultValueDefinitionForm {...props}>
           <FormField
-            control={state.control}
-            name={`min`}
+            control={props.state.control}
+            name={`defaultValue`}
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Minimum</FormLabel>
+                <FormLabel isRequired={false}>Default value</FormLabel>
                 <FormControl>
                   <Input
                     {...field}
-                    {...state.register('min', {
+                    {...props.state.register('defaultValue', {
                       setValueAs: setValueAsNumber,
                     })}
                     type="number"
                   />
                 </FormControl>
                 <FormDescription>
-                  The minimum Value the user is able to enter.
+                  The initial value for the field.
                 </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <FormField
-            control={state.control}
-            name={`max`}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Maximum</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    {...state.register('max', {
-                      setValueAs: setValueAsNumber,
-                    })}
-                    type="number"
-                  />
-                </FormControl>
-                <FormDescription>
-                  The maximum Value the user is able to enter.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+
+          <div className="flex flex-row items-center justify-between space-x-2">
+            <FormField
+              control={props.state.control}
+              name={`min`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel isRequired={false}>Minimum</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      {...props.state.register('min', {
+                        setValueAs: setValueAsNumber,
+                      })}
+                      type="number"
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    The minimum Value the user is able to enter.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={props.state.control}
+              name={`max`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel isRequired={false}>Maximum</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      {...props.state.register('max', {
+                        setValueAs: setValueAsNumber,
+                      })}
+                      type="number"
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    The maximum Value the user is able to enter.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </DefaultValueDefinitionForm>
 
         <FormField
-          control={state.control}
-          name={`isRequired`}
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-center justify-between rounded-lg border border-zinc-200 dark:border-zinc-800 p-3 shadow-sm">
-              <div>
-                <FormLabel>Required</FormLabel>
-                <FormDescription>
-                  Required fields need to be filled before a CollectionItem can
-                  be created or updated.
-                </FormDescription>
-                <FormMessage />
-              </div>
-              <FormControl>
-                <Switch
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={state.control}
+          control={props.state.control}
           name={`isUnique`}
           render={({ field }) => (
             <FormItem className="flex flex-row items-center justify-between rounded-lg border border-zinc-200 dark:border-zinc-800 p-3 shadow-sm">
               <div>
-                <FormLabel>Unique</FormLabel>
+                <FormLabel isRequired={true}>Unique</FormLabel>
                 <FormDescription>
                   You won't be able to create an Entry if there is an existing
                   Entry with identical content.
@@ -175,51 +123,6 @@ const NumberValueDefinitionForm = React.forwardRef<
             </FormItem>
           )}
         />
-
-        <FormField
-          control={state.control}
-          name={`isDisabled`}
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-center justify-between rounded-lg border border-zinc-200 dark:border-zinc-800 p-3 shadow-sm">
-              <div>
-                <FormLabel>Disabled</FormLabel>
-                <FormDescription>
-                  You won't be able to change the Value if this is active.
-                </FormDescription>
-                <FormMessage />
-              </div>
-              <FormControl>
-                <Switch
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        {/* <FormField
-          control={state.control}
-          name={`inputWidth`}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Field width</FormLabel>
-              <FormControl>
-                <Select onValueChange={field.onChange} {...field}>
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Select an icon" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="3">3/12</SelectItem>
-                    <SelectItem value="4">4/12</SelectItem>
-                    <SelectItem value="6">6/12</SelectItem>
-                    <SelectItem value="12">12/12</SelectItem>
-                  </SelectContent>
-                </Select>
-              </FormControl>
-            </FormItem>
-          )}
-        /> */}
       </form>
     </Form>
   );
@@ -243,15 +146,17 @@ const NumberValueDefinitionFormFieldExample = React.forwardRef<
       name={'example'}
       render={({ field }) => (
         <FormItem>
-          <FormLabel>{state.watch(`name.${currentLanguage}`)}</FormLabel>
+          <FormLabel isRequired={state.watch('isRequired')}>
+            {state.watch(`name.${currentLanguage}`)}
+          </FormLabel>
           <FormControl>
             <Input
-              {...field}
               {...state.register(field.name, { setValueAs: setValueAsNumber })}
               className={cn('bg-white dark:bg-zinc-900', className)}
               type="number"
               min={state.watch('min')}
               max={state.watch('max')}
+              defaultValue={state.watch('defaultValue')}
               required={state.watch('isRequired')}
               disabled={state.watch('isDisabled')}
             />
