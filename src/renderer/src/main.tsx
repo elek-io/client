60bf4562efa56047c0d1b522e1020f295573c58d
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-07-13 00:14:42 +0200

    Updated all dependencies to latest versions

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,22 +1,17 @@
-import {
-  BrowserProfilingIntegration,
-  BrowserTracing,
-  Replay,
-  init as sentryElectronRendererInit,
-} from '@sentry/electron/renderer';
+import * as Sentry from '@sentry/electron/renderer';
 import { init as sentryReactInit } from '@sentry/react';
 import './app';
 
-sentryElectronRendererInit(
+Sentry.init(
   {
     dsn: 'https://c839d5cdaec666911ba459803882d9d0@o4504985675431936.ingest.sentry.io/4506688843546624',
     integrations: [
-      new BrowserTracing(),
-      new BrowserProfilingIntegration(),
-      new Replay({
+      Sentry.browserTracingIntegration(),
+      Sentry.browserProfilingIntegration(),
+      Sentry.replayIntegration({
         maskAllText: true,
         blockAllMedia: true,
-      }),
+      })
     ],
     sampleRate: 1.0, // For error events @todo change this to a lower number once more people are using Client
     tracesSampleRate: 1.0, // For tracing events @todo change this to a lower number once more people are using Client
