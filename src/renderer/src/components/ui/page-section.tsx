863aaed28877249455ce2be4dedc7caabe1f7020
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-08-21 18:02:38 +0200

    Increased margin between page section header and content

--- src/renderer/src/components/ui/page-section.tsx
+++ src/renderer/src/components/ui/page-section.tsx
@@ -13,7 +13,7 @@ export interface PageSectionProps extends VariantProps<typeof styles> {
 export function PageSection(props: PageSectionProps): JSX.Element {
   return (
     <section className="p-6 border-t border-zinc-200 dark:border-zinc-800">
-      <div className="sm:flex sm:items-center">
+      <div className="sm:flex sm:items-center mb-4">
         <div className="sm:flex-auto">
           <h2 className="text-base font-semibold leading-6">{props.title}</h2>
           <p className="mt-2 text-[0.8rem] text-zinc-500 dark:text-zinc-400">
