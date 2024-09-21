5f77fed306531198cca5627efee621ff4b9b10f0
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-09-21 02:07:48 +0200

    GIFs can now be rendered as a static image without the animation or with it

--- src/renderer/src/components/ui/asset-display.tsx
+++ src/renderer/src/components/ui/asset-display.tsx
@@ -1,5 +1,6 @@
 import { Asset } from '@elek-io/core';
 import { FileQuestion, FolderArchive } from 'lucide-react';
+import { forwardRef, useEffect, useRef } from 'react';
 
 export interface AssetDisplayProps extends Asset {
   preview?: boolean;
@@ -7,6 +8,42 @@ export interface AssetDisplayProps extends Asset {
 
 export function AssetDisplay(props: AssetDisplayProps): JSX.Element {
   const absolutePath = 'elek-io-local-file://' + props.absolutePath;
+
+  if (props.mimeType === 'image/gif' && props.preview === false) {
+    const ref = useRef(null);
+    const gif = new Image();
+    gif.src = absolutePath;
+
+    const StaticGifCanvas = forwardRef<HTMLCanvasElement>((props, ref) => {
+      if (typeof ref === 'function') {
+        throw new Error(
+          `Only React Refs that are created with createRef or useRef are supported`
+        );
+      }
+
+      useEffect(() => {
+        const context = ref?.current?.getContext('2d');
+        if (!context) {
+          return undefined;
+        }
+
+        context.drawImage(gif, 0, 0);
+      }, []);
+
+      return (
+        <canvas
+          ref={ref}
+          {...props}
+          width={gif.width}
+          height={gif.height}
+          className="max-w-full max-h-full object-contain"
+        />
+      );
+    });
+    StaticGifCanvas.displayName = 'StaticGifCanvas';
+
+    return <StaticGifCanvas ref={ref} />;
+  }
 
   if (props.mimeType.startsWith('image/')) {
     return (
@@ -20,7 +57,12 @@ export function AssetDisplay(props: AssetDisplayProps): JSX.Element {
 
   if (props.mimeType.startsWith('video/')) {
     return (
-      <video muted controls={props.preview} autoPlay={props.preview}>
+      <video
+        muted
+        controls={props.preview}
+        autoPlay={props.preview}
+        src={absolutePath}
+      >
         <source src={absolutePath} type={props.mimeType}></source>
       </video>
     );
