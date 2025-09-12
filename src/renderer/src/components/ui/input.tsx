25260f0ebc81ba06a618f2d279373a8a8a8c2488
Nils Kolvenbach <info@nils-kolvenbach.de>
2025-09-12 13:31:17 +0200

    Changed inputs to not use opacity when disabled. Also use brand color on switch and slider

--- src/renderer/src/components/ui/input.tsx
+++ src/renderer/src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = forwardRef<
   return (
     <input
       className={cn(
-        'flex h-9 w-full rounded-md border border-zinc-200 bg-transparent px-3 py-1 text-sm shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-brand-600 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:placeholder:text-zinc-400',
+        'flex h-9 w-full rounded-md border border-zinc-200 bg-transparent px-3 py-1 text-sm shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-brand-600 disabled:cursor-not-allowed dark:border-zinc-800 dark:placeholder:text-zinc-400',
         className
       )}
       ref={ref}
--- src/renderer/src/components/ui/textarea.tsx
+++ src/renderer/src/components/ui/textarea.tsx
@@ -8,7 +8,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'flex min-h-[60px] w-full rounded-md border border-zinc-200 bg-transparent px-3 py-2 text-sm shadow-xs placeholder:text-zinc-500 focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-brand-600 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:placeholder:text-zinc-400',
+          'flex min-h-[60px] w-full rounded-md border border-zinc-200 bg-transparent px-3 py-2 text-sm shadow-xs placeholder:text-zinc-500 focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-brand-600 disabled:cursor-not-allowed dark:border-zinc-800 dark:placeholder:text-zinc-400',
           className
         )}
         ref={ref}
