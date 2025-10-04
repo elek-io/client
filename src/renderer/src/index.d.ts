12680023ba224f76d0b649293ea9f9c9d91e9750
Nils Kolvenbach <info@nils-kolvenbach.de>
2025-10-04 02:34:57 +0200

    Fixed ts errors of fontsource

--- src/renderer/src/index.d.ts
+++ src/renderer/src/index.d.ts
@@ -1,3 +1,8 @@
+// @see https://fontsource.org/docs/getting-started/faq#typescript-errors
+declare module '*.css';
+declare module '@fontsource/*' {}
+declare module '@fontsource-variable/*' {}
+
 // @see https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forward_and_create_ref/#option-3---call-signature
 interface ForwardRefWithGenerics extends FC<WithForwardRefProps<Option>> {
   <T extends Option>(
