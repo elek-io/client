d8995e301c8d59a8fce1c934fdf288d1c699698b
Nils Kolvenbach <info@nils-kolvenbach.de>
2025-11-10 01:43:55 +0100

    Added shadcn sidebar and skeleton components. Modified sidebar to be as simple as possible.

---
+++ src/renderer/components/ui/skeleton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { cn } from '@renderer/lib/utils';
+
+function Skeleton({
+  className,
+  ...props
+}: React.ComponentProps<'div'>): React.JSX.Element {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn('animate-pulse rounded-md bg-accent', className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
