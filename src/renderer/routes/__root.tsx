a8f4b369446504a37e47499511806576f4eaa9d5
Nils Kolvenbach <info@nils-kolvenbach.de>
2025-11-17 12:24:56 +0100

    Removed IPC from route context

--- src/renderer/routes/__root.tsx
+++ src/renderer/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import {
   type ErrorComponentProps,
   Outlet,
@@ -14,7 +15,7 @@ import { Button } from '@renderer/components/ui/button';
 import { ScrollArea, ScrollBar } from '@renderer/components/ui/scroll-area';
 import { Toaster } from '@renderer/components/ui/sonner';
 
-export interface RouterContext extends ContextBridgeApi {}
+export interface RouterContext {}
 
 // Use the routerContext to create your root route
 export const Route = createRootRouteWithContext<RouterContext>()({
@@ -25,9 +26,8 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 
 function ErrorComponent({ error }: ErrorComponentProps): ReactElement {
   const router = useRouter();
-  const { electron, core } = Route.useRouteContext();
 
-  core.logger.error({
+  window.ipc.core.logger.error({
     source: 'desktop',
     message: `Uncaught route error: ${error.message}`,
     meta: { error: { message: error.message, stack: error.stack } },
@@ -64,7 +64,7 @@ function ErrorComponent({ error }: ErrorComponentProps): ReactElement {
 
   return (
     <>
-      <AppHeader electron={electron} />
+      <AppHeader electron={window.ipc.electron} />
       <Page title="Error" description={<Description />} actions={<Actions />}>
         <div className="p-6">
           <p>{error.message}</p>
@@ -82,7 +82,6 @@ function ErrorComponent({ error }: ErrorComponentProps): ReactElement {
 
 function NotFoundComponent(): ReactElement {
   const router = useRouter();
-  const { electron } = Route.useRouteContext();
 
   function Description(): ReactElement {
     return <>You&apos;ve tried accessing a route that could not be found.</>;
@@ -108,7 +107,7 @@ function NotFoundComponent(): ReactElement {
 
   return (
     <>
-      <AppHeader electron={electron} />
+      <AppHeader electron={window.ipc.electron} />
       <Page
         title="Not Found"
         description={<Description />}
@@ -125,14 +124,13 @@ function NotFoundComponent(): ReactElement {
 }
 
 function RootComponent(): ReactElement {
-  const { electron } = Route.useRouteContext();
-
   return (
     <>
-      <AppHeader electron={electron} />
+      <AppHeader electron={window.ipc.electron} />
       <Outlet></Outlet>
       <Toaster />
-      <TanStackRouterDevtools position="bottom-right" />
+      <TanStackRouterDevtools position="bottom-right" initialIsOpen={false} />
+      <ReactQueryDevtools position="bottom" initialIsOpen={false} />
     </>
   );
 }
