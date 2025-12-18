7da7008651c2790db43fe371deb59c35a2805b45
Nils Kolvenbach <info@nils-kolvenbach.de>
2025-12-18 14:41:52 +0100

    Re-added redirect to User profile when trying to access any project related page without a setup User. This is needed for many git operations

--- src/renderer/routes/projects.tsx
+++ src/renderer/routes/projects.tsx
@@ -1,10 +1,20 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 import type { ReactElement } from 'react';
 
 import { useBreadcrumb } from '@renderer/hooks/useBreadcrumb';
+import { queryClient, queryOptions } from '@renderer/queries';
 
 export const Route = createFileRoute('/projects')({
   component: ProjectsLayout,
+  loader: async () => {
+    const user = await queryClient.ensureQueryData(queryOptions.user.get());
+
+    if (!user) {
+      throw redirect({
+        to: '/user/profile',
+      });
+    }
+  },
 });
 
 function ProjectsLayout(): ReactElement {
